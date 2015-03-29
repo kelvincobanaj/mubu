9a5b48e0a4a55b94313811b8f975e5311fe911d3
Kelvin Çobanaj <kelvincobanaj22@gmail.com>
2015-03-30 00:22:35 +0200

    Initial Commit

---
+++ Crinita/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Crinita
+//
+//  Created by Kelvin Çobanaj on 30/03/15.
+//  Copyright (c) 2015 Spiranca. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
